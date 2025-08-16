7fc9adac3afab5da5a1dfebc55295b32c4e20c1d
SHAIK AMEER <shaikameerjann@gmail.com>
2025-08-16 20:08:10 +0530

    Lawer Appointment Application

---
+++ LawyerAppointmentApplication/src/main/java/com/example/LawyerAppointmentApplication/LawyerAppointmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.LawyerAppointmentApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawyerAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawyerAppointmentApplication.class, args);
+	}
+
+}
---
+++ LawyerAppointmentApplication/src/test/java/com/example/LawyerAppointmentApplication/LawyerAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LawyerAppointmentApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawyerAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
